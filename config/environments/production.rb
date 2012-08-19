440d30bb65466dd1362f89fae21cade9914789ca
David Chua <zhchua@gmail.com>
2012-08-19 17:23:34 +0800

    Add default host parameters to environment config files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Kandan::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.action_mailer.default_url_options = { :host => "superfriendscollectivechat.herokuapp.com" }
+
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
